a233db451cbef242a8a7c7934d3a74466f47605a
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-23 14:29:15 +0100

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -13,23 +13,13 @@ class Logger {
         const timestamp = new Date().toISOString();
         const formattedMessage = args.length ? util.format(message, ...args) : message;
         const logEntry = `[${timestamp}] [${level}] ${formattedMessage}\n`;
-        
         console.log(`[${level}] ${formattedMessage}`);
         fs.appendFileSync(this.logFile, logEntry);
     }
 
-    info(message, ...args) {
-        this.write('INFO', message, ...args);
-    }
-
-    warn(message, ...args) {
-        this.write('WARN', message, ...args);
-    }
-
-    error(message, ...args) {
-        this.write('ERROR', message, ...args);
-    }
-
+    info(message, ...args) { this.write('INFO', message, ...args); }
+    warn(message, ...args) { this.write('WARN', message, ...args); }
+    error(message, ...args) { this.write('ERROR', message, ...args); }
     debug(message, ...args) {
         if (process.env.DEBUG) {
             this.write('DEBUG', message, ...args);
